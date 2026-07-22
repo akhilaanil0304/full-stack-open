99f245618a0103ad9620794933105f16d04c6a41
Akhila Anil <akhilaanil0304@gmail.com>
2026-07-22 23:17:28 +0530

    Complete Unicafe app (exercises 1.6-1.11)

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+)
+
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+
+  // 1.9: Display conditional message if no feedback has been cast yet
+  if (total === 0) {
+    return <p>No feedback given</p>
+  }
+
+  const average = (good * 1 + neutral * 0 + bad * -1) / total
+  const positivePercentage = (good / total) * 100
+
+  // 1.11: Render statistics in an HTML table layout
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average.toFixed(1)} />
+        <StatisticLine text="positive" value={`${positivePercentage.toFixed(1)} %`} />
+      </tbody>
+    </table>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+
+      <h2>statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
